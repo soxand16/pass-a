b551622a56c3b73c257fec08d6014b5d637d1d13
Samuel Oxandale <soxand16@gmail.com>
2025-01-06 07:37:41 -0800

    added deck class with draw and shuffle

--- simulation.py
+++ simulation.py
@@ -1,3 +1,21 @@
+import random
 from collections import namedtuple
 
 Card = namedtuple('Card', ['value', 'suit'])
+
+
+class Deck():
+
+    def __init__(self):
+        self._cards = [Card(value, suit) for value in range(1, 14)
+                       for suit in ['S', 'D', 'C', 'H']
+                       ]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def draw(self):
+        return self._cards.pop(0)
+
+    def shuffle(self):
+        random.shuffle(self._cards)
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from simulation import Card
+from simulation import Card, Deck
 
 class TestCard(unittest.TestCase):
 
@@ -13,6 +13,44 @@ class TestCard(unittest.TestCase):
                 test = Card(value, suit)
                 self.assertEqual(test.value, value)
                 self.assertEqual(test.suit, suit)
+
+
+class TestDeck(unittest.TestCase):
+
+    def setup(self):
+        pass
+
+    def testMakeDeck(self):
+        _ = Deck()
+
+    def testDeckHas52Cards(self):
+        deck = Deck()
+        self.assertEqual(len(deck), 52)
+
+    def testDeckHasAllUniqueCards(self):
+        deck = Deck()
+        for value in range(1, 14):
+            for suit in ['S', 'D', 'C', 'H']:
+                test = Card(value, suit)
+                self.assertTrue(test in deck._cards)
+
+    def testDrawCard(self):
+        deck = Deck()
+        self.assertTrue(isinstance(deck.draw(), Card))
+
+    def testDrawRemovesCardFromDeck(self):
+        deck = Deck()
+        for i in range(51, 0):
+            deck.draw()
+            self.assertEqual(len(deck), i)
+
+    def testShuffleDeck(self):
+        deck = Deck()
+        initial_order = deck._cards.copy()
+        deck.shuffle()
+        shuffled_order = deck._cards
+        self.assertNotEqual(shuffled_order, initial_order)
+
 
 if __name__ == '__main__':
     unittest.main()
